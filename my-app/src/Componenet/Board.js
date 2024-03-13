d1e150ad8151f4d9637f8e16f66e5e195f2eec63
samiksha-3010 <samiksha30102@gmail.com>
2024-03-13 16:15:39 +0530

    updates

---
+++ my-app/src/Componenet/Board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Board.css"
+import Box from './Box'
+
+
+export const Board = ({ board, onClick }) => {
+      return (
+        <div className="board">
+          {
+            board.map((value, idx) => {
+              return <Box value={value} onClick={() => value === null && onClick(idx)} />;
+            })
+          }
+        </div>
+      )
+    }
+
+export default Board
